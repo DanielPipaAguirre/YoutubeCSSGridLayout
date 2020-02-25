762c583504086a650c200f3c5781e2668e5bce1f
Daniel Pipa <danielpipa21@gmail.com>
2020-02-24 20:43:17 -0500

    Responsive Design

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 bar.addEventListener('click', () => {
     page.classList.toggle("active");
 })
+const comprobarAncho = () => {
+    if (window.innerWidth <= 768) {
+        page.classList.remove("active");
+    } else {
+        page.classList.add("active");
+    }
+}
+comprobarAncho();
+window.addEventListener("resize", () => {
+    comprobarAncho();
+})
